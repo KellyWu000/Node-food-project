ade683abcb17bf1e7320de9636d365cbcf0db1aa
Nicolas Liu <st880517@gmail.com>
2021-10-29 01:33:36 +0800

    20211029_CartAdd

---
+++ models/Cart.js
@@ -0,0 +1,194 @@
+const db = require("./../modules/connect-mysql");
+
+const tableName = "order_temp";
+const tableMember="member_detail";
+const tableOrderList="order_list";
+const pkField = "Order_Sid";
+const MemberField="Member_id";
+
+class Cart {
+  constructor(defaultObj = {}) {
+    this.data = defaultObj;
+  }
+
+  // 取得 Order_Temp 所有商品
+    static async getFullList() {
+    // const sql =  `SELECT ot.*,od.* FROM ${tableName} ot LEFT JOIN order_detail od ON ot.Order_Sid=od.Order_sid;
+    // `;
+    const sql=`SELECT ot.Order_Sid,ot.Product_id,ot.Order_Amount,pf.name,pf.price,pf.cate_sid FROM ${tableName} ot LEFT JOIN products_food pf ON ot.Product_id=pf.product_id`;
+    const [rs] = await db.query(sql);
+    return rs;
+  }
+
+  // 讀取 Order_Temp 商品 id 
+  static async getList(Order_Sid) {
+    const sql = `SELECT * FROM ${tableName} WHERE ${pkField}=?`;
+    const [rs] = await db.query(sql,[Order_Sid]);
+    if (rs && rs.length === 1) {
+      return new Cart(rs[0]);
+    }
+    return null;
+  }
+
+
+  // 加入購物車
+   static async addtmpList(Order_Sid,Member_id,Product_id,Order_Amount) {
+    const output = {
+      success: false,
+      error: "",
+    };
+
+    // 不要重複輸入資料
+    if (await Cart.getList(Order_Sid)) {
+      output.error = "資料重複了";
+      return output;
+    }
+
+    //參數都必須要有資料
+    const obj = {
+      Order_Sid,
+      Member_id,
+      Product_id,
+      Order_Amount,
+    };
+
+    const sql = `INSERT INTO ${tableName} SET ?`;
+    const [r] = await db.query(sql, [obj]);
+
+    output.success = !!r.affectedRows ? true : false;
+
+    // 確認是否有成功加入購物車
+    output.cartList=await Cart.getList();
+    return output;
+   }
+
+ 
+  // 更新購物車
+  static async updatetmpList(Order_Sid,Order_Amount) {
+    const output = {
+      success: false,
+      error: "",
+    };
+
+    //參數都必須要有資料
+    const obj = {
+      // Member_id,
+      // Product_id,
+      Order_Amount,
+    };
+    const sql = `UPDATE ${tableName} SET ? WHERE ${pkField}=?`;
+    const [r] = await db.query(sql, [obj, Order_Sid]);
+
+    output.success = !!r.affectedRows ? true : false;
+
+    return output;
+   }
+
+  // 刪除購物車單一品項
+  static async removetmpList(Order_Sid){
+    const output={
+      success:false,
+      error:'',
+    }
+    const sql = `DELETE FROM ${tableName} WHERE ${pkField}=?`;
+    const [r] = await db.query(sql, [Order_Sid]);
+    output.success = !!r.affectedRows ? true : false;
+
+    return output;
+  }
+
+  // 清空購物車
+  static async TruncatetmpList(){
+    const output = {
+      success: false,
+      error: "",
+    };
+    const sql = `TRUNCATE TABLE ${tableName}`;
+    const [r] = await db.query(sql);
+  }
+
+
+  // 新增 Member_Detail
+  static async addListDetail(Sid,Member_id,Order_Name,Order_Phone,E_Mail,Order_Address,Invoice_Type,Payment_Type,Order_Remark) {
+
+    const output = {
+      success: false,
+      error: "",
+    };
+
+       // 不要重複輸入資料
+      //  if (await Cart.getList(Order_Sid)) {
+      //   output.error = "資料重複了";
+      //   return output;
+      // }
+
+          //參數都必須要有資料
+    const obj = {
+      Sid,
+      Member_id,
+      Order_Name,
+      Order_Phone,
+      E_Mail,
+      Order_Address,
+      Invoice_Type,
+      Payment_Type,
+      Order_Remark
+    };
+
+    const sql = `INSERT INTO ${tableMember} SET ?`;
+    const [r] = await db.query(sql,[obj]);
+    output.success = !!r.affectedRows ? true : false;
+
+     // 確認是否有成功加入購物車
+    //  output.cartList=await Cart.getList();
+
+     return output;
+  }
+
+   // 讀取 Member_Detail 訂購人 
+   static async getListDetail(Member_id) {
+    const sql = `SELECT * FROM ${tableMember} WHERE ${MemberField}=?`;
+    const [rs] = await db.query(sql,[Member_id]);
+    if (rs && rs.length === 1) {
+      return new Cart(rs[0]);
+    }
+    return null;
+  }
+
+
+  // 新增 Order_List
+  static async ConfirmList(Order_Sid,Member_id,Total_Price,Order_Status) {
+    const output = {
+      success: false,
+      error: "",
+    };
+
+       // 不要重複輸入資料
+      //  if (await Cart.getList(Order_Sid)) {
+      //   output.error = "資料重複了";
+      //   return output;
+      // }
+
+          //參數都必須要有資料
+    const obj = {
+      Order_Sid,
+      Member_id,
+      Total_Price,
+      Order_Status,
+    };
+
+    const sql = `INSERT INTO ${tableOrderList} SET ?`;
+    const [r] = await db.query(sql,[obj]);
+    output.success = !!r.affectedRows ? true : false;
+
+     // 確認是否有成功加入購物車
+    //  output.cartList=await Cart.getList();
+
+     return output;
+  }
+}
+
+module.exports = Cart;
+
+
+			
---
+++ routes/cart.js
@@ -0,0 +1,159 @@
+const express = require("express");
+const { getList } = require("../models/Cart");
+const Cart = require("./../models/Cart");
+
+const router = express.Router();
+
+// router.use((req, res, next) => {
+//   // 判斷有沒有通過 jwt 驗證
+//   if (req.myAuth && req.myAuth.id) {
+//     next();
+//   } else {
+//     res.json({success:'false',error:"沒有 token 或者 token 不合法"});
+//   }
+// });
+
+// 新增項目(老師)
+// router.post("/", async (req, res) => {
+// // req.body.product_id
+// // req.body.quantity
+
+// const output={
+//   success:'false',
+//   error:''
+// }
+
+// res.json(await Cart.add(req.myAuth.id, req.body.product_id,req.body.quantity))
+// });
+
+// 修改項目(老師)
+// router.put("/:id", async (req, res) => {
+//   res.json(await Cart.update(req.myAuth.id, req.body.product_id,req.body.quantity))
+
+// });
+
+
+// 讀取單筆 Order_Temp
+router.get("/:id", async (req, res) => {
+  res.json(await Cart.getList(req.params.id))
+});
+
+
+// 讀取所有 Order_Temp
+router.get("/", async (req, res) => {
+  res.json(await Cart.getFullList())
+});
+
+
+
+// 新增 Order_Temp
+router.post("/", async (req, res) => {
+  // req.body.product_id
+  // req.body.quantity
+  
+  const output={
+    success:'false',
+    error:''
+  }
+  
+  res.json(await Cart.addtmpList(
+    req.body.Order_Sid,
+    req.body.Member_id,
+    req.body.Product_id,
+    req.body.Order_Amount))
+  });
+
+// 修改 Order_Temp
+router.put("/:id", async (req, res) => {
+  res.json(await Cart.updatetmpList(
+    req.params.id,
+    // req.body.Member_id,
+    // req.body.Product_id,
+    req.body.Order_Amount,
+    ))
+
+});
+
+// 刪除 Order_Temp
+router.delete("/:id", async (req, res) => {
+  res.json(await Cart.removetmpList(
+    req.params.id,
+    ))
+
+});
+
+//清空 Order_Temp
+router.delete("/", async (req, res) => {
+  res.json(await Cart.TruncatetmpList());
+
+});
+
+
+// 讀取單筆 Order_Detail
+router.get("/addList/:id", async (req, res) => {
+  res.json(await Cart.getListDetail(req.params.id))
+});
+
+
+// 新增 Order_Detail
+router.post("/addList", async (req, res) => {
+   
+    
+    const output={
+      success:'false',
+      error:''
+    }
+    
+    res.json(await Cart.addListDetail(
+      req.body.Sid,
+      req.body.Member_id,
+      req.body.Order_Name,
+      req.body.Order_Phone,
+      req.body.E_Mail,
+      req.body.Order_Address,
+      req.body.Invoice_Type,
+      req.body.Payment_Type,
+      req.body.Order_Remark,))
+  });
+
+
+  // 新增 Order_List
+  router.post("/ConfirmList", async (req, res) => {
+   
+    const output={
+      success:'false',
+      error:''
+    }
+    
+    res.json(await Cart.ConfirmList(
+      req.body.Order_Sid,
+      req.body.Member_id,
+      req.body.Total_Price,
+      req.body.Order_Status,
+    ))
+  });
+  
+
+
+
+// // 刪除項目(老師)
+// router.delete("/:id", async (req, res) => {
+//   res.json(await Cart.remove(req.myAuth.id, req.body.product_id,req.body.quantity))
+// });
+
+
+
+// (老師)
+// router.delete("/", async (req, res) => {
+//   res.json(await Cart.clear(req.myAuth.id))
+
+// });
+
+// router.delete("/", async (req, res) => {
+//   res.json(await Cart.clear())
+
+// });
+
+
+
+module.exports = router;
