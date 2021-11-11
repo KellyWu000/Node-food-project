fc83392b0cc65238830aafac3e3b2ba2a86d72ed
Una Chang <una.c303@outlook.com>
2021-11-11 16:07:24 +0800

    member point revised

--- routes/ArtFood.js
+++ routes/ArtFood.js
@@ -62,9 +62,13 @@ router.post('/QA/:id', async (req, res) => {
 
     if (result.affectedRows === 1 && result.changedRows === 1) {
 
-
         // SELECT SUM(`change_point`) c_points  FROM `member_point` WHERE `member_sid`=53
-        const sql1 = "SELECT SUM(`change_point`) c_points  FROM `member_point` WHERE `member_sid`=?";
+        const sqlsub = "SELECT `change_point` sub_points  FROM `member_point` WHERE `change_type`='USE' AND `member_sid`=?";
+        const sql1 = "SELECT SUM(`change_point`) c_points  FROM `member_point` WHERE `change_type`='GET' AND `member_sid`=?";
+
+        const [rssub] = await db.query(sqlsub, [req.myAuth.memberid]);
+        const sub_points = Number(rssub[0].sub_points);
+
         const [rs1] = await db.query(sql1, [req.myAuth.memberid]);
         const c_points = Number(rs1[0].c_points);
 
@@ -72,24 +76,11 @@ router.post('/QA/:id', async (req, res) => {
         //const [result2] = await db.query(sql2, [c_points+1, req.myAuth.memberid]);
 
 
-
-//                const pointSql = 
-
-// "               INSERT INTO `member_point` SET `change_point` = 1, `left_point` =1, `change_reason`='專欄問答點數',`change_type`='GET',`member_sid` = ?; UPDATE `member_point` SET `change_point` = `change_point`, `left_point` =(SELECT SUM(CASE WHEN `member_sid` =? THEN `change_point` END) `left_point`  FROM member_point)  WHERE `member_sid` =? ";
-
-
-// SELECT `member_sid`=53, INSERT INTO `member_point` SET `change_point` = 1, SUM(`change_point`)AS`left_point`, `change_reason`='專欄問答點數test',`change_type`='GET', `member_sid` = 53
-
-
-
-            //    "UPDATE `member_point` SET `change_point` = `change_point` + 1, `left_point` = `left_point` + 1 WHERE `member_sid` = ?"
-       
-
         let pointResult;
     
         try {
             //[pointResult] = await db.query(pointSql, [req.myAuth.memberid]);
-            [pointResult] = await db.query(sql2, [c_points+1, req.myAuth.memberid]);
+            [pointResult] = await db.query(sql2, [c_points-sub_points+1, req.myAuth.memberid]);
             output.success = true;
         } catch (ex) {
             output.pointError = ex.toString();
